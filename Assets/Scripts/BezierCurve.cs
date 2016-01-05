551acd267999e7a1e827a6feb52fc4986d652cea
Luke <luke.thompson@commonmode.co.uk>
2016-01-05 16:10:02 +0000

    Incorporated parent handle scale into local/global stuff. Moved all gizmo display code to Curve, not Node. Added node axis drawing. Simplified some stuff.

--- Assets/Scripts/BezierCurve.cs
+++ Assets/Scripts/BezierCurve.cs
@@ -10,27 +10,88 @@ public class BezierCurve : MonoBehaviour {
 	public float strengthScale {get {return _strengthScale;}}
 
 #if UNITY_EDITOR
-	[Header("Preview")]
+	[Header("Handles Preview")]
 	[SerializeField]
 	private float _previewSegments = 10;
 	[SerializeField]
 	private float _handleScale = 1;
 	public float handleSize {get {return _handleScale * 0.1f;}}
-	[SerializeField]
+	//[SerializeField]
 	private Color _curveColor = Color.magenta;
 	public Color curveColor {get {return _curveColor;}}
-	[SerializeField]
+	//[SerializeField]
 	private Color _handleColor = Color.green;
 	public Color handleColor {get {return _handleColor;}}
 	[SerializeField]
 	private HandleType _handleWidget = HandleType.SPHERE;
 	public HandleType handleWidget {get {return _handleWidget;}}
+	[Header("Node Preview")]
+	[SerializeField]
+	private bool _showAxes = true;
+	[SerializeField]
+	private float _axesScale = 1;
 
 	public void OnDrawGizmosSelected(){
 		BezierNode[] nodes = GetComponentsInChildren<BezierNode>();
+		Color gcol = Gizmos.color;
+
+		// Draw handle vectors and caps per node
+		// Handle position gizmos are drawn by BezierEditor
 		foreach (BezierNode bn in nodes){
-			bn.DrawGizmos();
+			Gizmos.color = handleColor;
+			Gizmos.DrawLine(bn.transform.position, bn.h1);
+			Gizmos.DrawLine(bn.transform.position, bn.h2);
+			
+			System.Action<Vector3> drawSolid = null;
+			System.Action<Vector3> drawWire = null;
+			
+			// Select handle primitive
+			switch (handleWidget){
+			case BezierCurve.HandleType.CUBE:
+				drawSolid = (h) => {
+					Gizmos.DrawCube(h, Vector3.one * handleSize);
+				};
+				drawWire = (h) => {
+					Gizmos.DrawWireCube(h, Vector3.one * handleSize);
+				};
+				break;
+			case BezierCurve.HandleType.SPHERE:
+				drawSolid = (h) => {
+					Gizmos.DrawSphere(h, handleSize);
+				};
+				drawWire = (h) => {
+					Gizmos.DrawWireSphere(h, handleSize);
+				};
+				break;
+			}
+			
+			// Select combination of solid/wireframe according to symmetry, and draw gizmos
+			switch (bn.symmetry){
+			case BezierNode.Symmetry.FULL:
+				drawSolid(bn.h1);
+				drawSolid(bn.h2);
+				break;
+			case BezierNode.Symmetry.ANGLE:
+				drawSolid(bn.h1);
+				drawWire(bn.h2);
+				break;
+			case BezierNode.Symmetry.NONE:
+				drawWire(bn.h1);
+				drawWire(bn.h2);
+				break;
+			}
+
+			if (_showAxes){
+				Gizmos.color = Color.blue;
+				Gizmos.DrawLine(bn.transform.position, bn.transform.position + (bn.transform.right * _axesScale));
+				Gizmos.DrawLine(bn.transform.position, bn.transform.position - (bn.transform.right * _axesScale));
+				Gizmos.color = Color.red;
+				Gizmos.DrawLine(bn.transform.position, bn.transform.position + (bn.transform.forward * _axesScale));
+			}
 		}
+
+		// Draw spline
+		Gizmos.color = curveColor;
 		for (int i=0; i<nodes.Length-1; ++i){
 			BezierNode prev = nodes[i];
 			BezierNode next = nodes[i+1];
@@ -42,11 +103,12 @@ public class BezierCurve : MonoBehaviour {
 			for (int j=0; j<_previewSegments; ++j){
 				t += 1f/_previewSegments;
 				nextPos = Bezier(t, prev.transform.position, prev.h2, next.h1, next.transform.position);
-				Gizmos.color = curveColor;
 				Gizmos.DrawLine(lastPos, nextPos);
 				lastPos = nextPos;
 			}
 		}
+
+		Gizmos.color = gcol;
 	}
 #endif
 
--- Assets/Scripts/BezierNode.cs
+++ Assets/Scripts/BezierNode.cs
@@ -3,14 +3,13 @@ using System.Collections;
 
 [ExecuteInEditMode]
 public class BezierNode : MonoBehaviour {
-	private enum Symmetry {FULL, ANGLE, NONE}
+	public enum Symmetry {FULL, ANGLE, NONE}
 
 	#region Settings
 	[SerializeField]
-	private float _strengthScale = 1;
-	[SerializeField]
 	private Symmetry _symmetry = Symmetry.FULL;
 	private Symmetry _lastSymmetry = Symmetry.FULL;
+	public Symmetry symmetry {get {return _symmetry;}}
 	#endregion
 
 	#region Handle properties
@@ -18,26 +17,22 @@ public class BezierNode : MonoBehaviour {
 	private Vector3 _h1 = new Vector3(0,0,-1f);
 	public Vector3 h1 {
 		get {
-			return transform.TransformPoint(_h1);
+			return LocalToGlobal(_h1);
 		}
 		set {
-			_h1 = transform.InverseTransformPoint(value);
-			DoSymmetry(_h1,_h2,(h)=>{
-				h2=h;
-			});
+			_h1 = GlobalToLocal(value);
+			DoSymmetry(_h1, ref _h2);
 		}
 	}
 	[SerializeField][HideInInspector]
 	private Vector3 _h2 = new Vector3(0,0,1f);
 	public Vector3 h2 {
 		get {
-			return transform.TransformPoint(_h2);
+			return LocalToGlobal(_h2);
 		}
 		set {
-			_h2 = transform.InverseTransformPoint(value);
-			DoSymmetry(_h2,_h1,(h)=>{
-				h1=h;
-			});
+			_h2 = GlobalToLocal(value);
+			DoSymmetry(_h2, ref _h1);
 		}
 	}
 	#endregion
@@ -60,27 +55,27 @@ public class BezierNode : MonoBehaviour {
 	}
 	#endregion
 
-	#region Handle manipulation methods
-	private void DoSymmetry(Vector3 master, Vector3 slave, System.Action<Vector3> setProperty){
+	#region Handle Manipulation Methods
+	private Vector3 LocalToGlobal(Vector3 lHandle){
+		return transform.TransformPoint(parent.strengthScale * lHandle);
+	}
+	private Vector3 GlobalToLocal(Vector3 gHandle){
+		return transform.InverseTransformPoint(gHandle) / parent.strengthScale;
+	}
+	private void DoSymmetry(Vector3 master, ref Vector3 slave){
 		Symmetry hm = _symmetry;
 		_symmetry = Symmetry.NONE;	// Temporarily disable symmetry to avoid recursion
 		switch (hm){
 		case Symmetry.FULL:
 			// Mirror entire local vector
-			setProperty( transform.TransformPoint(-master) );
+			slave = -master;
 			break;
 		case Symmetry.ANGLE:
 			// Mirror local vector, then scale to original length
-			setProperty( transform.TransformPoint(-master * (slave.magnitude / master.magnitude)) );
+			slave = (-master * (slave.magnitude / master.magnitude));
 			break;
 		}
 		_symmetry = hm;
-	}
-	private Vector3 LocalToGlobal(Vector3 handle){
-		return transform.InverseTransformPoint(transform.localPosition + (handle * _strengthScale * parent.strengthScale));
-	}
-	private Vector3 GlobalToLocal(Vector3 pos){
-		return transform.TransformPoint((pos - transform.position) / (_strengthScale * parent.strengthScale));
 	}
 	#endregion
 
@@ -89,62 +84,10 @@ public class BezierNode : MonoBehaviour {
 		// Tell parent to draw gizmos. Parent will tell this node and all others to DrawGizmos.
 		parent.OnDrawGizmosSelected();
 	}
-	/// <summary>
-	/// Draw bezier handle gizmos. Does NOT draw movement axis gizmos - done by BezierEditor
-	/// Should be called only by BezierCurve parent.
-	/// </summary>
-	public void DrawGizmos(){
-		Color gcol = Gizmos.color;
-		Gizmos.color = parent.handleColor;
-		Gizmos.DrawLine(transform.position, h1);
-		Gizmos.DrawLine(transform.position, h2);
-
-		System.Action<Vector3> drawSolid = null;
-		System.Action<Vector3> drawWire = null;
-
-		// Select handle primitive
-		switch (parent.handleWidget){
-		case BezierCurve.HandleType.CUBE:
-			drawSolid = (h) => {
-				Gizmos.DrawCube(h, Vector3.one * parent.handleSize);
-			};
-			drawWire = (h) => {
-				Gizmos.DrawWireCube(h, Vector3.one * parent.handleSize);
-			};
-			break;
-		case BezierCurve.HandleType.SPHERE:
-			drawSolid = (h) => {
-				Gizmos.DrawSphere(h, parent.handleSize);
-			};
-			drawWire = (h) => {
-				Gizmos.DrawWireSphere(h, parent.handleSize);
-			};
-			break;
-		}
-
-		// Select combination of solid/wireframe according to symmetry, and draw gizmos
-		switch (_symmetry){
-		case Symmetry.FULL:
-			drawSolid(h1);
-			drawSolid(h2);
-			break;
-		case Symmetry.ANGLE:
-			drawSolid(h1);
-			drawWire(h2);
-			break;
-		case Symmetry.NONE:
-			drawWire(h1);
-			drawWire(h2);
-			break;
-		}
-		Gizmos.color = gcol;
-	}
 	private void Update(){
 		if (_symmetry != _lastSymmetry){
 			_lastSymmetry = _symmetry;
-			DoSymmetry(_h1, _h2, (h)=>{
-				h2 = h;
-			});
+			DoSymmetry(_h1, ref _h2);
 		}
 	}
 #endif
